f703933431ad43d0ae68bb864b26c70525f76053
Matt McClean <matt.mcclean@gmail.com>
2013-04-08 23:30:22 +0200

    Update default.rb to OpsWorks default values

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,8 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default["tomcat"]["port"] = 8080
-default["tomcat"]["ssl_port"] = 8443
+default["tomcat"]["port"] = 80
+default["tomcat"]["ssl_port"] = 443
 default["tomcat"]["ajp_port"] = 8009
 default["tomcat"]["java_options"] = "-Xmx128M -Djava.awt.headless=true"
 default["tomcat"]["use_security_manager"] = false
